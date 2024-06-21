cc9727c8ec8ecfa79c103489dada9d4c2af3c8f8
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-06-21 12:44:46 +0200

    Fonctions sans paramètre

---
+++ notions-intermediaires/fonctions/fonctions-sans-parametres.php
@@ -0,0 +1,11 @@
+<!-- Une fonction sans argument ne prend aucun paramètre lorsqu'elle est appellée 
+Elles sont souvent utilisées pour effectuer des tâches simples et répétitives 
+qui ne necessitent pas de données en entrée -->
+
+<?php 
+function afficherMessage() {
+    echo "Bonjour!";
+}
+
+afficherMessage();
+?>
