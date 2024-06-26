e9231cb17aafc0ea8c8d179bbe9277b7b1e043da
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-06-26 12:19:45 +0200

    Variables de base et variable POST

---
+++ notions-de-base/variables/variables-post.php
@@ -0,0 +1,25 @@
+<!-- Les variables POST contiennent des données envoyées par un formulaire lorsqu'un utilisateur en soumet un.
+Les données sont envoyées au serveur PHP via la méthode POST.
+Les données sont ensuite stockées dans des variables spéciales appellées variables POST  -->
+
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="traitement.php" method="POST">
+        <label for="nom">Nom : </label>
+        <input type="text" id="nom" name="nom">
+        <label for="email">E-mail : </label>
+        <input type="email" id="email" name="email">
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
---
+++ notions-de-base/variables/variables.php
@@ -0,0 +1,11 @@
+ <!-- Les bases des variables -->
+
+ <?php 
+
+$age = 28;
+$prenom = "Pénélope";
+$solde = 50.30;
+$message = "Bonjour " .$prenom." !";
+
+echo $message;
+ ?>
