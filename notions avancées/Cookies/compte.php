315c45ffa2c717c6db276870e266741ba5dfbf95
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-07-15 15:35:43 +0200

    Supprimer un cookie

--- notions avancées/Cookies/compte.php
+++ notions avancées/Cookies/compte.php
@@ -12,6 +12,7 @@ setcookie("username", $nom,time()+3600, "/","", false, true);
 $nom = isset($_COOKIE['username'])?$_COOKIE['username']:$nom;
 
 if(isset($_GET['logout'])) {
+    setcookie("username", "", time()-3600,"/", "", false, true); // Suppression du cookie (donc la récup en ligne 12 n'est plus activée)
     session_destroy();
     header('Location: delete.php');
     exit();
