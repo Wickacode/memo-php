5073ecc7ef1dd1523a937dbb5a129066d5bb6d7a
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-07-18 09:55:19 +0200

    correction du fichier sur les objets

--- notions avancées/objets-intro/index.php
+++ notions avancées/objets-intro/index.php
@@ -5,27 +5,30 @@ Pour créer une instance de classe on va créer le mot clé "New" suivi du nom d
 des arguments pour le constructeur de la classe.
 -->
 
-<?php 
+<?php
 
 //Création de la classe Person avec deux propriétés public name et public age
-class Person {
+class Person
+{
     public $name;
     public $age;
 
-    public function__construct($name, $age){
-        $this->name= $name;
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
         $this->age = $age;
     }
 
     //Méthode greet qui affiche une salutation pesonnalisée en utilisant les propriétés de l'objet.
-    public function greet() {
-        echo "Hello je suis".this->name."et j'ai ".this->age." ans";
+    public function greet()
+    {
+        echo "Hello je m'appelle " . $this->name . " et j'ai " . $this->age . " ans";
     }
-
-    //Création d'une instance de classe Person en utilisant le constructeur de classe et en passant 
-    //les arguments John et 30.
-    $person = new Person("John", 30);
-    //Appel de la méthode greet sur cette instance
-    $person->greet();
 }
+//Création d'une instance de classe Person en utilisant le constructeur de classe et en passant 
+//les arguments John et 30.
+$person = new Person("John", 30);
+//Appel de la méthode greet sur cette instance
+$person->greet();
+
 ?>
